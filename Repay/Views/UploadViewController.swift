01f555f3c7c9f080481244f06f7e4263bef81bd4
Kevin Vincent <kevinsundar@gmail.com>
2016-04-08 22:44:17 -0700

    Controller files

---
+++ Repay/Views/UploadViewController.swift
@@ -0,0 +1,38 @@
+//
+//  UploadViewController.swift
+//  Repay
+//
+//  Created by Kevin Vincent on 4/8/16.
+//  Copyright © 2016 Esha Joshi. All rights reserved.
+//
+
+import UIKit
+
+class UploadViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func cancel(sender: AnyObject) {
+    self.dismissViewControllerAnimated(true, completion: {});
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
